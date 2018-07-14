bc4e0df428538ad2eaf2b0453ae47c1e2e75a5cd
bill.wang <wangzhijiebill@gmail.com>
2018-07-14 16:13:14 -0700

    stage

--- pkg/garbledcircuits/circuit.go
+++ pkg/garbledcircuits/circuit.go
@@ -1,5 +1,32 @@
 package garbledcircuit
 
+// A CryptoFunc is any function that takes a value and a key and
+// returns an encrypted or decrypted value.
+type CryptoFunc func(uint32, uint32) uint32
+
+// A Wire represents the wire between two gates.
+// currently assuming 2 parties, hosting 2 keys
+type Wire struct {
+	Input  Gate      // the input gate
+	Output Gate      // the output gate
+	Keys   [2]uint32 // the keys:one for 0, one for 1P
+	uint32           // a randomised p-value
+}
+
+// Input 'gate', used to supply inputs to the circuit.
+type Input struct {
+	Value   uint32
+	circuit *Circuit // a pointer back to the Circuit
+	Output  *Wire    // the output wire
+}
+
+// A Gate represents any binary or unary gate.
+type Gate interface {
+	Evaluate() (uint32, uint32)
+	Circuit() *Circuit
+	GetOutput() *Wire
+}
+
 // referencing implmentation from https://github.com/JoelOtter/go-garbled/blob/master/circuit.go
 
 //Circuit struct for
@@ -38,4 +65,32 @@ func (c *Circuit) AddInput(name string) *Input {
 	i.circuit = c
 	c.Inputs[name] = i
 	return i
+}
+
+// AddOutput registers a ew new Output in Output in the circuit
+// with the provided name
+
+func (c *Circuit) addOutput(name string, g Gate) {
+	c.Outputs[name] = g.GetOutput()
+}
+
+// Evaluate wille valuate a whole circuite for the inputs specifiged
+// in the map 'inputs'. Returns a map of outputs to their values.NewCircuit
+//
+// E.g. for a circuit containing a single AND gate with
+// inputs A and B, and one output 0, the map:
+// {"A" :0, "B":1}
+// will evaluate to :
+// {'0":0'}
+
+func (c *Circuit) Evaluate(inputs map[string]uint32) map[string]uint32 {
+	for k, v := range inpputs {
+		c.Inputs[k].Value = v
+	}
+	outputs := make(map[string]uinst32)
+	for k, v := range c.Outputs {
+		_, outputP := v.Evaluate()
+		outputs[k] = outputP ^ v.P
+	}
+	return outputs
 }
